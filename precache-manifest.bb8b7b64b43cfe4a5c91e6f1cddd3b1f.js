c854db11eb46c21c594cb05f0fea82e5accd9211
PepeFiStach <adamkowski.m95@gmail.com>
2018-10-15 21:15:55 +0200

    Updates

---
+++ precache-manifest.bb8b7b64b43cfe4a5c91e6f1cddd3b1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "09f9d692a2b8f4da28d6b07bcbc4f062",
+    "url": "/Paint-React/static/media/pencil-white.09f9d692.png"
+  },
+  {
+    "revision": "72493d13de2fd8c57ba4",
+    "url": "/Paint-React/static/css/main.0e94500f.chunk.css"
+  },
+  {
+    "revision": "c79029ca61c5754e37a9",
+    "url": "/Paint-React/static/js/1.c79029ca.chunk.js"
+  },
+  {
+    "revision": "cb62902f786f9c358883",
+    "url": "/Paint-React/static/js/runtime~main.cb62902f.js"
+  },
+  {
+    "revision": "c3992041b6c3f9ffc5d488d5509b196c",
+    "url": "/Paint-React/static/media/pencil.c3992041.jpg"
+  },
+  {
+    "revision": "b10cd0bb8dbd5ee5bae6a656120a6a03",
+    "url": "/Paint-React/static/media/move.b10cd0bb.jpg"
+  },
+  {
+    "revision": "72493d13de2fd8c57ba4",
+    "url": "/Paint-React/static/js/main.72493d13.chunk.js"
+  },
+  {
+    "revision": "1fdf0851ca5fab435f2ed564f00f6511",
+    "url": "/Paint-React/static/media/ruber.1fdf0851.png"
+  },
+  {
+    "revision": "329c531db0e1db5ed05293204d8dadcb",
+    "url": "/Paint-React/static/media/move-white.329c531d.jpg"
+  },
+  {
+    "revision": "8c702c1622bdec49438251e6c0107172",
+    "url": "/Paint-React/static/media/ruber-white.8c702c16.png"
+  },
+  {
+    "revision": "0fc36b2a7c3e5f9e0a23f66de3ecb600",
+    "url": "/Paint-React/static/media/color-wheel.0fc36b2a.png"
+  },
+  {
+    "revision": "38076e588d1101b27222d9f288f58873",
+    "url": "/Paint-React/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/Paint-React/precache-manifest.bb8b7b64b43cfe4a5c91e6f1cddd3b1f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Paint-React/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
