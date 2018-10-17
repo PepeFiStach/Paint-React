baa6edc9edd225b36094a3ff1145aa929549da0f
PepeFiStach <adamkowski.m95@gmail.com>
2018-10-17 14:57:22 +0200

    Another test commit for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -98,6 +98,7 @@ export default class AppTest extends React.Component {
                           this.state.sizeEraser, this.state.shape);
     console.log(this.getStage);
     console.log(this.state.mode);
+    console.log('Test for gh-pages');
     if (this.state.shape === 'rect' 
         || this.state.shape === 'star' 
         || this.state.shape === 'arrow'
